8bb86aafb6514c28bc1df085d4ef2f43e7323b76
hyungji.k <icthelight@naver.com>
2021-10-08 22:32:11 +0900

    POR-19 slick 라이브러리로 works, about 컴포넌트 수정

--- src/views/about/components/KoreanCV.js
+++ src/views/about/components/KoreanCV.js
@@ -1,11 +1,35 @@
 import styled from 'styled-components';
 
 import Section from "../../home/components/shared/Section";
+import AboutSlick from "./Slick/AboutSlick";
+
 
 const KoreanCV = () => {
   return (
     <Container>
-      KoreanCV
+      <Content>
+        <h1>
+          안녕하세요, 신입 프론트엔드 개발자 김형지입니다.
+        </h1>
+        <p>
+          사용자들이 이용하기에 편리하도록 웹페이지를 개발 및 개편하고 더 편리한 새로운 기능을 추가하는 등의 모습을 보며 프론트엔드 직무를 동경하게 되었습니다.
+        </p>
+        <p>
+          사진을 검색하고 필터링하는 <span className={'important'}>Unsplash</span>,
+          동영상을 검색하는 <span className={'important'}>Youtube </span>
+          등의 사이트를
+          <span className={'important'}> ReactJS</span>를 기반으로
+          <span className={'important'}> Redux (Redux-Saga, Redux-toolkit)</span> 을 활용하여 제작했습니다.
+        </p>
+        <p>
+          프로젝트를 진행하며 <span className={'important'}>Jira, Fork, Github, ESLint</span> 등을 적극 활용하며 타겟을 맞추고, 애자일한 방식으로 업무하는 것에 익숙해지려
+          노력했습니다.
+        </p>
+        <p>
+          다양한 프로젝트를 통해 계속해서 발전하는 프론트엔드 개발자가 되고 싶습니다.
+        </p>
+        <AboutSlick/>
+      </Content>
     </Container>
   )
 }
@@ -13,4 +37,26 @@ const KoreanCV = () => {
 const Container = styled(Section)`
 
 `;
+
+const Content = styled.div`
+  text-align: center;
+  padding: 0 40px;
+  font-size: 18px;
+  line-height: 40px;
+  color: #222426;
+
+  h1 {
+    font-weight: bold;
+  }
+
+  span {
+    &.important {
+      background: linear-gradient(to right top, rgba(0, 173, 181, 0.9), #00344a);
+      color: transparent;
+      -webkit-background-clip: text;
+      font-weight: bold;
+    }
+  }
+`;
+
 export default KoreanCV;
---
+++ src/views/home/components/Works/Slick/ProjectsSlick.js
@@ -0,0 +1,150 @@
+import React, {Component} from "react";
+import styled from 'styled-components';
+import Image from 'next/image';
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import {ContentContainer} from "../../../../shared/components/LayoutStyled";
+import {works} from "../../../../../data/works";
+import {RiGithubFill, RiGlobalLine} from "react-icons/ri";
+
+export default class ProjectsSlick extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+        <ContentContainer>
+          <StyledSlider {...settings}
+          >
+            {works.list.map(item => {
+              return (
+                <div key={item.name}>
+                  <Item key={item.name} className={'item'}>
+                    <Thumb>
+                      <Image src={item.thumbnail}/>
+                    </Thumb>
+                    <Desc>
+                      <Title>{item.name}</Title>
+                      <Content>{item.desc}</Content>
+                    </Desc>
+                    <IconBox>
+                      <Icon href={item.sourceUrl}>
+                        <RiGithubFill/>
+                        <h3>View Source</h3>
+                      </Icon>
+                      <Icon href={item.siteUrl}>
+                        <RiGlobalLine/>
+                        <h3>Visit Website</h3>
+                      </Icon>
+                    </IconBox>
+                  </Item>
+                </div>
+              );
+            })}
+          </StyledSlider>
+        </ContentContainer>
+      </div>
+    );
+  }
+}
+
+const StyledSlider = styled(Slider)`
+  .slick-slide div {
+    outline: none;
+  }
+`;
+
+const ImageContainer = styled.div`
+  margin: 0 16px;
+
+  img {
+    width: 100%;
+    height: 250px;
+    object-fit: cover;
+  }
+
+  p {
+    font-size: 17px;
+  }
+`;
+
+const Item = styled.div`
+ 
+`;
+
+const Thumb = styled.div`
+  display: flex;
+  justify-content: center;
+  img {
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+  }
+`;
+
+const Desc = styled.div`
+  padding: 10px 0;
+`;
+
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  font-weight: 600;
+  color: rgb(0, 173, 172);
+  margin-bottom: 10px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  color: #111;
+  font-weight: bold;
+`;
+
+const IconBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Icon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid rgb(0, 173, 172);
+  border-radius: 10px;
+  padding: 10px;
+  margin: 10px;
+  color: rgb(0, 173, 172);
+  font-weight: 600;
+  cursor: pointer;
+  transition: 0.3s;
+
+  svg {
+    width: 30px;
+    height: 30px;
+    fill: rgb(0, 173, 172);
+    transition: 0.3s;
+
+  }
+
+  &:hover {
+    background: #fff;
+    border: 1px solid rgb(53,98,123);
+    color: rgb(53,98,123);
+    svg {
+      fill: rgb(53,98,123);
+    }
+  }
+
+`;
